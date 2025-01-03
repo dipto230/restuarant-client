5995b22b6e7dff319c96c74b68f2e46f4bf740b3
dipto230 <diptoroy230305@gmail.com>
2025-01-03 18:05:47 +0600

    featured section done

---
+++ src/pages/Home/Home/Featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import SectionTittle from '../../../../components/SectionTittle/SectionTittle';
+import featured from '../../../../assets/home/featured.jpg'
+import './Featured.css';
+
+const Featured = () => {
+    return (
+        <div className='featured-item my-20 pt-8'>
+        <SectionTittle
+            heading="Featured Item"
+            subHeading="Check it out"
+        />
+        <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+            <div>
+                <img src={featured} alt=''/>
+            </div>
+            <div className='md:ml-10 text-white '>
+            <p>March 20, 2023</p>
+        <p className='uppercase'>WHERE CAN I GET SOME?</p>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
+           <button className='btn btn-outline'>Read More </button> 
+            </div>
+        </div>
+        </div>
+        
+       
+      
+       
+    );
+};
+
+export default Featured;
