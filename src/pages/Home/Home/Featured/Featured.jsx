acaf52a0606a011e907ed1bba45eb86eb8501dab
dipto230 <diptoroy230305@gmail.com>
2025-01-03 18:09:51 +0600

    parallax section done

--- src/pages/Home/Home/Featured/Featured.jsx
+++ src/pages/Home/Home/Featured/Featured.jsx
@@ -5,12 +5,12 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className='featured-item my-20 pt-8'>
+        <div className='featured-item bg-fixed my-20 pt-8'>
         <SectionTittle
             heading="Featured Item"
             subHeading="Check it out"
         />
-        <div className='md:flex justify-center items-center pb-20 pt-12 px-36'>
+        <div className='md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36'>
             <div>
                 <img src={featured} alt=''/>
             </div>
@@ -19,7 +19,7 @@ const Featured = () => {
         <p className='uppercase'>WHERE CAN I GET SOME?</p>
         <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-           <button className='btn btn-outline'>Read More </button> 
+           <button className='btn btn-outline border-0 border-b-4 mt-4'>Read More </button> 
             </div>
         </div>
         </div>
